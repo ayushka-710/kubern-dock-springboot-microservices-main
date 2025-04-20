63a67682709569ba10869ce3686c80eee42ae39e
ayushka-710 <ayushkakharel1010@gmail.com>
2025-04-20 22:46:09 +0545

    latest-update

--- backup/apm-tutorial-java-host/notes/src/main/java/com/datadog/example/notes/NotesApplication.java
+++ backup/apm-tutorial-java-host/notes/src/main/java/com/datadog/example/notes/NotesApplication.java
@@ -14,5 +14,17 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class NotesApplication {
     public static void main(String[] args) {
         SpringApplication.run(NotesApplication.class, args);
+	
+	//Redis connection test
+	// UnifiedJedis jedis = new UnifiedJedis("redis://192.168.0.107:6379");
+
+	// String pong = jedis.ping();
+        // System.out.println("Connected to Redis: " + pong);  // should print "PONG"
+
+        // jedis.set("foo", "bar");
+        // System.out.println("Value of 'foo': " + jedis.get("foo"));
+
+        // jedis.close();
+
     }
 }
